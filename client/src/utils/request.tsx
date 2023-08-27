72719ee7aa23500a21f0e2a062eab5a32960cdd9
QuangAnh22112003 <quanganhbh1@gmail.com>
2023-08-27 17:12:38 +0700

    Update request.tsx

--- client/src/utils/request.tsx
+++ client/src/utils/request.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'https://time-planner-test.vercel.app/',
+    baseURL: 'https://time-planner-123.vercel.app/',
     withCredentials: true, // Cho phép gửi cookie
 });
 export default request;
